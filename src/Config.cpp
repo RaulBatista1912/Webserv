b887975dbf11d0e0c685d0fa54d437ceb8b2b662
daniel <daniel-congo2@hotmail.com>
2026-03-09 16:28:40 +0100

    rebuild the Makefile and folders

--- src/Config.cpp
+++ src/Config.cpp
@@ -5,50 +5,50 @@
 #include <cstdlib>
 
 Config::Config(const std::string& path) {
-    ParseFile(path);
+	ParseFile(path);
 }
 
-const   std::vector<ServerConfig>& Config::getServers() const {
-    return (servers);
+const std::vector<ServerConfig>& Config::getServers() const {
+	return (servers);
 }
 
-void    Config::ParseFile(const std::string& path) {
-    std::ifstream file(path.c_str());
-    if (!file.is_open()) {
-        std::cerr << "Error: cannot open config file\n";
-        std::exit(1);
-    }
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.find("server") != std::string::npos) {
-            ParseServerBlock(file);
-        }
-    }
-    if (servers.empty()) {
-        std::cerr << "Error: no server block found\n";
-        std::exit(1);
-    }
+void Config::ParseFile(const std::string& path) {
+	std::ifstream file(path.c_str());
+	if (!file.is_open()) {
+		std::cerr << "Error: cannot open config file\n";
+		std::exit(1);
+	}
+	std::string line;
+	while (std::getline(file, line)) {
+		if (line.find("server") != std::string::npos) {
+			ParseServerBlock(file);
+		}
+	}
+	if (servers.empty()) {
+		std::cerr << "Error: no server block found\n";
+		std::exit(1);
+	}
 }
 
-void    Config::ParseServerBlock(std::ifstream& file) {
-    ServerConfig server;
-    std::string line;
+void Config::ParseServerBlock(std::ifstream& file) {
+	ServerConfig server;
+	std::string line;
 
-    while (std::getline(file, line)) {
-        if (line.find("listen") != std::string::npos) {
-            std::stringstream ss(line);
-            std::string word;
-            ss >> word;
-            ss >> word;
-            server.port = std::atoi(word.c_str());
-        }
-        if (line.find("}") != std::string::npos)
-            break;
-    }
-    if (server.port <= 0) {
-        std::cerr << "Error: invalid listen port\n";
-        std::exit(1);
-    }
-    servers.push_back(server);
+	while (std::getline(file, line)) {
+		if (line.find("listen") != std::string::npos) {
+			std::stringstream ss(line);
+			std::string word;
+			ss >> word;
+			ss >> word;
+			server.port = std::atoi(word.c_str());
+		}
+		if (line.find("}") != std::string::npos)
+			break;
+	}
+	if (server.port <= 0) {
+		std::cerr << "Error: invalid listen port\n";
+		std::exit(1);
+	}
+	servers.push_back(server);
 }
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -5,36 +5,36 @@
 #include <stdexcept>
 
 Server::Server(int port) : fd(-1), port(port) {
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0)
-        throw std::runtime_error("socket failed");
-    int opt = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    sockaddr_in addr; // struct IPv4
-    std::memset(&addr, 0, sizeof(addr)); // initialise a 0
-    addr.sin_family = AF_INET; // IPv4
-    addr.sin_addr.s_addr = INADDR_ANY; // accepte toutes les interfaces reseau
-    addr.sin_port = htons(port); // definit port, htons() converti host -> network byte order
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0)
+		throw std::runtime_error("socket failed");
+	int opt = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	sockaddr_in addr; // struct IPv4
+	std::memset(&addr, 0, sizeof(addr)); // initialise a 0
+	addr.sin_family = AF_INET; // IPv4
+	addr.sin_addr.s_addr = INADDR_ANY; // accepte toutes les interfaces reseau
+	addr.sin_port = htons(port); // definit port, htons() converti host -> network byte order
 
-    if (bind(fd, (sockaddr*)&addr, sizeof(addr)) < 0) // associe le socket serveur a l'addresse IP + port
-        throw std::runtime_error("bind failed");
-    if (listen(fd, 10) < 0) // met le socket TCP en mode passif pour accepet des connexions et creer une gile d'attente des clients en attente
-        throw std::runtime_error("listen failed"); 
+	if (bind(fd, (sockaddr*)&addr, sizeof(addr)) < 0) // associe le socket serveur a l'addresse IP + port
+		throw std::runtime_error("bind failed");
+	if (listen(fd, 10) < 0) // met le socket TCP en mode passif pour accepet des connexions et creer une gile d'attente des clients en attente
+		throw std::runtime_error("listen failed"); 
 }
 
 Server::~Server() {
-    if (fd >= 0)
-        close(fd);
+	if (fd >= 0)
+		close(fd);
 }
 
 int Server::getFd() const {
-    return (fd);
+	return (fd);
 }
 
 int Server::getPort() const {
-    return (port);
+	return (port);
 }
 
 int Server::acceptClient() const {
-    return (accept(fd, NULL, NULL));
+	return (accept(fd, NULL, NULL));
 }

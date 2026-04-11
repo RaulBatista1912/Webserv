752d763bbecd8008724ecc1b1a333ce7f0dc8eed
Raul Batista <rabatist@c2r9s6.42lausanne.ch>
2026-04-11 17:50:19 +0200

    ups

--- includes/Cgi.hpp
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include "../includes/Client.hpp"
-
-static std::string	findCgiContentType(const std::string& cgiOutput);
-HttpResult			handleCGI(const std::string& path, const ServerConfig* server, const Location* loc);
